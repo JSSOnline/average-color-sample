cdbf230e471657ade92d8f5f76e0b346f75a3971
Jason Slabbers <jslabbers@gmail.com>
2019-04-09 16:56:55 +0200

    Added Boostrap + nav

--- js/main.js
+++ js/main.js
@@ -1,28 +1,5 @@
-// function avgImageColors(){
-//     var img = document.createElement('img');
-//     img.setAttribute('src', 'images/people-q-c-640-480-7.jpg')
-//     img.addEventListener('load', function() {
-//         var vibrant = new Vibrant(img, 64, 5);
-//         var swatches = vibrant.swatches()
-//         for (var swatch in swatches){
-//             if (swatches.hasOwnProperty(swatch) && swatches[swatch]){
-//                 let colors = document.getElementsByClassName('colors');
-//                 let color = document.createElement('div');
-//                 color.style.backgroundColor = swatches[swatch].getHex();
-
-//                 if (swatch == 'Vibrant') {
-//                     document.body.style.backgroundColor = swatches[swatch].getHex();
-//                 }
-
-//                 colors[0].appendChild(color);
-//                 console.log(swatch, swatches[swatch].getHex())
-//             }
-//         }
-//     });
-// }
-
 function avgImageColors(){
-    var img = document.getElementsByTagName('img')[0];
+    var img = document.getElementsByTagName('img')[1];
     console.log(img)
     var vibrant = new Vibrant(img, 64, 3);
     var swatches = vibrant.swatches()
@@ -32,7 +9,7 @@ function avgImageColors(){
             let color = document.createElement('div');
             color.style.backgroundColor = swatches[swatch].getHex();
 
-            if (swatch == 'LightVibrant') {
+            if (swatch == 'Vibrant') {
                 document.body.style.backgroundColor = swatches[swatch].getHex();
             }
             colors[0].appendChild(color);
